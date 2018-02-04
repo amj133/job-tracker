657d049cc23f87ebbdc091762751e9314846f704
amj133 <amj@vt.edu>
2018-02-04 13:54:19 -0700

    created categories index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,25 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all
+  end
 
-end 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+
+    flash.notice = "Category '#{@category.title}' Created!"
+
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
+end
