2402ebae377ca27e32af2f951590cd2977ee7b98
amj133 <amj@vt.edu>
2018-02-04 15:51:02 -0700

    user can edit category passing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,8 +18,19 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def edit
+  def show
+    @category = Category.find(params[:id])
+  end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+
+    redirect_to category_path(@category)
   end
 
   def destroy
