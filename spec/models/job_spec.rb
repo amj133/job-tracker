b5637e9d335ce74a20471218458fb381bc76f116
amj133 <amj@vt.edu>
2018-02-05 15:18:07 -0700

    job model test passing

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -4,26 +4,60 @@ describe Job do
   describe "validations" do
     context "invalid attributes" do
       it "is invalid without a title" do
-        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
+        job = Job.new(level_of_interest: 80,
+                      description: "Wahoo",
+                      city: "Denver",
+                      company_id: 1,
+                      category_id: 1)
         expect(job).to be_invalid
       end
 
       it "is invalid without a level of interest" do
-        job = Job.new(title: "Developer", description: "Wahoo", city: "Denver")
+        job = Job.new(title: "Developer",
+                      description: "Wahoo",
+                      city: "Denver",
+                      company_id: 1,
+                      category_id: 1)
         expect(job).to be_invalid
       end
 
       it "is invalid without a city" do
-        job = Job.new(title: "Developer", description: "Wahoo", level_of_interest: 80)
+        job = Job.new(title: "Developer",
+                      description: "Wahoo",
+                      level_of_interest: 80,
+                      company_id: 1,
+                      category_id: 1)
+        expect(job).to be_invalid
+      end
+
+      it "is invalid without a company" do
+        job = Job.new(title: "Developer",
+                      description: "Wahoo",
+                      city: "Denver",
+                      level_of_interest: 80,
+                      category_id: 1)
+        expect(job).to be_invalid
+      end
+
+      it "is invalid without a category" do
+        job = Job.new(title: "Developer",
+                      description: "Wahoo",
+                      city: "Denver",
+                      level_of_interest: 80,
+                      company_id: 1)
         expect(job).to be_invalid
       end
     end
 
     context "valid attributes" do
-      it "is valid with a title, level of interest, company, and category" do
-        category = Category.new(title: "red")
-        company = Company.new(name: "Turing")
-        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver", company: company, category_id: 1)
+      it "is valid with a title, level of interest, city, company, and category" do
+        category = Category.create!(title: "red")
+        company = Company.create!(name: "Turing")
+        job = Job.new(title: "Developer",
+                      level_of_interest: 40,
+                      city: "Denver",
+                      company_id: company.id,
+                      category_id: category.id)
         expect(job).to be_valid
       end
     end
@@ -31,11 +65,6 @@ describe Job do
 
   describe "relationships" do
     it { should belong_to(:company) }
+    it { should belong_to(:category)}
   end
-  #-----------BELOW FROM ORIGINAL
-  #   it "belongs to a company" do
-  #     job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
-  #     expect(job).to respond_to(:company)
-  #   end
-  # end
 end
