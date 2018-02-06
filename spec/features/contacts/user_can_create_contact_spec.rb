0b7e29a7b902c299c21daa4113945b9ca580b230
amj133 <amj@vt.edu>
2018-02-06 15:38:11 -0700

    user can create new contact passing

--- spec/features/contacts/user_can_create_contact_spec.rb
+++ spec/features/contacts/user_can_create_contact_spec.rb
@@ -4,17 +4,25 @@ describe "user can create new contact" do
   context "visits company show page" do
     it "can create new contact from company page" do
       company = Company.create!(name: "Walden")
+      category = Category.create!(title: "blue")
+      company.contacts.create!(full_name: "Sally Blue",
+                               position: "Manager",
+                               email: "sallyblue@gmail.com")
+      company.jobs.create!(title: "Developer",
+                           level_of_interest: 70,
+                           city: "Denver",
+                           category_id: category.id)
 
       visit company_path(company)
       fill_in('contact[full_name]', with: "Johnny Moneymaker")
       fill_in('contact[position]', with: "BAMF")
       fill_in('contact[email]', with: "ilikefrogs@gmail.com")
-      fill_in('contact[company_id]', with: 1)
       click_on('Create Contact')
+      save_and_open_page
 
-      expect(current_path).to eq(company_path(company))
+      expect(current_path).to eq(company_jobs_path(company))
       expect(page).to have_content("Contacts:")
-      expect(page).to have_content("Contacts: Johnny Moneymaker - BAMF")
+      expect(page).to have_content("Johnny Moneymaker - BAMF")
     end
   end
 end
