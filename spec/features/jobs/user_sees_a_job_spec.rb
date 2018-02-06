39483d0b81b673916b88c802e679358e9d57b29d
amj133 <amj@vt.edu>
2018-02-05 18:03:11 -0700

    added user adds a comment to job show page feature test

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -16,5 +16,22 @@ describe "User sees a specific job" do
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
+    expect(page).to have_content(comment_1.body)
+    expect(page).to have_content(comment_2.body)
+  end
+
+  scenario "user adds a comment to the page" do
+    company = Company.create!(name: "ESPN")
+    category = Category.create!(title: "blue")
+    job = company.jobs.create!(title: "Developer",
+                               level_of_interest: 70,
+                               city: "Denver",
+                               category_id: category.id)
+
+    visit company_job_path(company, job)
+    fill_in('comment[body]', with: "Contacted Mr. Smith on 3/5, he said they are not actively seeking a developer but will keep my info on file.")
+    click_on('Submit')
+
+    expect(page).to have_content(job.comments.first.body)
   end
 end
