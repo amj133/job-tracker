c34ab32228347ae8935d6993cffb877f0352b2a0
amj133 <amj@vt.edu>
2018-02-07 06:54:49 -0700

    added dashboard feature test - user sees analytics - it displays count of jobs by level of interest

---
+++ spec/features/dashboard/user_sees_analytics_on_dashboard_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe "user sees analytics on dashboard" do
+  context "user visits dashboard" do
+    it "displays a count of jobs by level of interest" do
+      company_1 = Company.create!(name: "ESPN")
+      company_2 = Company.create!(name: "ABC")
+      category = Category.create!(title: "blue")
+      job_1 = Job.create!(title: "Designer",
+                          level_of_interest: 80,
+                          city: "Richmond",
+                          category_id: category.id,
+                          company_id: company_2.id)
+      job_2 = Job.create!(title: "Manager",
+                          level_of_interest: 70,
+                          city: "Albany",
+                          category_id: category.id,
+                          company_id: company_1.id)
+      job_3 = Job.create!(title: "Developer",
+                          level_of_interest: 60,
+                          city: "Denver",
+                          category_id: category.id,
+                          company_id: company_1.id)
+      job_4 = Job.create!(title: "Communicator",
+                          level_of_interest: 70,
+                          city: "Denver",
+                          category_id: category.id,
+                          company_id: company_1.id)
+
+      visit '/dashboard'
+
+      expect(page).to eq({80 => 1, 70 => 2, 60 => 1})
+      expect(page).to have_content("Level of Interest : Number of Jobs")
+      expect(page).to have_content("80 : 1")
+      expect(page).to have_content("70 : 2")
+      expect(page).to have_content("60 : 1")
+    end
+  end
+end
